9fcad82dce8e9ba5e20eec5f0c7cc8b21fe09056
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-10-18 19:58:12 +0700

    Создание билда

---
+++ src/test/java/DeliveryCardTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DeliveryCardTest {
+    @Test
+    void shouldDeliveryCardOrder() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Кемерово");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        String dateOfMeeting = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").setValue(dateOfMeeting);
+        $("[data-test-id=name] input").setValue("Павел-К");
+        $("[data-test-id=phone] input").setValue("+79238456245");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(11));
+        $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dateOfMeeting));
+    }
+}
