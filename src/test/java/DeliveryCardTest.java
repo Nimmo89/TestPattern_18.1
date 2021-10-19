859e5b1f94cc61ffb25d627d1c9322d258fc02fb
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-10-19 20:15:17 +0700

    test v3

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -27,28 +27,11 @@ public class DeliveryCardTest {
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         String dateOfMeetingReplan = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=date] input").setValue(dateOfMeetingReplan);
-        $("[data-test-id=agreement]").click();
+//        $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Запланировать")).click();
-        $(withText("Необходимо подтверждение")).shouldBe(visible);
+        $(withText("Необходимо подтверждение")).shouldBe(visible, Duration.ofSeconds(10));
         $("[data-test-id=replan-notification] .notification__content").shouldHave(ownText("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $("[data-test-id=replan-notification] .button").click();
-        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + dateOfMeeting));
+        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + dateOfMeetingReplan));
     }
-
-//    @Test
-//    void shouldDeliveryCardOrderOnOtherDate() {
-//        open("http://localhost:9999");
-//        $("[data-test-id=city] input").setValue("Кемерово");
-//        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-//        String dateOfMeeting = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-//        $("[data-test-id=date] input").setValue(dateOfMeeting);
-//        $("[data-test-id=name] input").setValue("Павел-К");
-//        $("[data-test-id=phone] input").setValue("+79238456245");
-//        $("[data-test-id=agreement]").click();
-//        $$("button").find(exactText("Запланировать")).click();
-//        $(withText("Необходимо подтверждение")).shouldBe(visible);
-//        $("[data-test-id=replan-notification] .notification__content").shouldHave(ownText("У вас уже запланирована встреча на другую дату. Перепланировать?"));
-//        $("[data-test-id=replan-notification] .button").click();
-//        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + dateOfMeeting));
-//    }
 }
