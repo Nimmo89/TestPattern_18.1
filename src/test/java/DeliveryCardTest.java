833569b0956752b5b70c963093399fed6751b09e
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-11-15 20:19:11 +0700

    Allure

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -1,4 +1,6 @@
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 
 import java.time.Duration;
@@ -9,24 +11,29 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryCardTest {
 
-    @Test
-    void shouldDeliveryCardOrder() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
-        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
-        $("[data-test-id=name] input").setValue(DataGenerator.generateName());
-        $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone());
-        $("[data-test-id=agreement]").click();
-        $$("button").find(exactText("Запланировать")).click();
-        $(withText("Успешно!")).shouldBe(visible);
-        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3)));
-        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(5));
-        $$("button").find(exactText("Запланировать")).click();
-        $(withText("Необходимо подтверждение")).shouldBe(visible, Duration.ofSeconds(10));
-        $("[data-test-id=replan-notification] .notification__content").shouldHave(ownText("У вас уже запланирована встреча на другую дату. Перепланировать?"));
-        $("[data-test-id=replan-notification] .button").click();
-        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(5)));
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
-}
+
+        @Test
+        void shouldDeliveryCardOrder () {
+            open("http://localhost:9999");
+            $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
+            $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+            $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
+            $("[data-test-id=name] input").setValue(DataGenerator.generateName());
+            $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone());
+            $("[data-test-id=agreement]").click();
+            $$("button").find(exactText("Запланировать")).click();
+            $(withText("Успешно!")).shouldBe(visible);
+            $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3)));
+            $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+            $("[data-test-id=date] input").setValue(DataGenerator.generateDate(5));
+            $$("button").find(exactText("Запланировать")).click();
+            $(withText("Необходимо подтверждение")).shouldBe(visible, Duration.ofSeconds(10));
+            $("[data-test-id=replan-notification] .notification__content").shouldHave(ownText("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+            $("[data-test-id=replan-notification] .button").click();
+            $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(5)));
+        }
+    }
